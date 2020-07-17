2f4d4fae00844801f0e912b4ba7114a23910bd41
Jonathan Chasse <jonathan.r.chasse@gmail.com>
2020-07-17 15:03:25 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,5 @@
+require "pry"
+
 class Song
   attr_accessor :name, :artist_name
   @@all = []
@@ -9,5 +11,63 @@ class Song
   def save
     self.class.all << self
   end
+
+  def self.create
+    song = self.new
+    self.all << song
+    song
+  end
+
+  def self.new_by_name(name)
+    song = self.new
+    song.name = name
+    song
+  end
+
+  def self.create_by_name(name)
+    song = self.new
+    song.name = name
+    self.all << song
+    song
+  end
+
+  def self.find_by_name(name)
+    self.all.find {|person| person.name == name}
+  end
+
+  def self.find_or_create_by_name(name)
+    if find_by_name(name) == nil
+      create_by_name(name)
+    else
+      find_by_name(name)
+    end
+  end
+
+  def self.alphabetical
+    self.all.sort_by do |song_inst|
+      song_inst.name
+    end
+  end
+
+  def self.new_from_filename (filename)
+    song = self.new
+    temp_array = filename.split(" - ")
+    # binding.pry
+    song.artist_name = temp_array[0]
+    song.name = temp_array[1].delete ".p3".chomp #need to figure out a way to remove the .mp3
+  end
+
+  def self.create_from_filename(filename)
+    # song = self.new
+    # self.all << song
+    # song
+  end
+
+
+  def self.destroy_all
+    self.all.clear
+  end
+
+
 
 end
